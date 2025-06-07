597b9c4be4b672a2ab4397a016d23567431bd7c1
Dani Vela <daniel@veladan.org>
2025-06-07 11:20:27 +0200

    mcpclient

--- Sources/mcpo-swift/MCPClient.swift
+++ Sources/mcpo-swift/MCPClient.swift
@@ -1,4 +1,5 @@
 import Foundation
+import OpenAI
 
 struct ToolInvocationRequest {
     let toolId: String
@@ -9,32 +10,6 @@ struct ToolInvocationRequest {
     }
 }
 
-public struct ToolInvocationRequest2: Codable {
-    let toolId: String
-    let parameters: [String: String]
-
-    // enum ToolInvocationRequestKeys: String, CodingKey {
-    //     case toolId, parameters
-    // }
-    
-    // public init(from decoder: Decoder) throws {
-    //     let container =  try decoder.container(keyedBy: ToolInvocationRequestKeys.self)
-    //     toolId = try container.decode(String.self, forKey: .toolId)
-    //     parameters = try container.decode(Any.self, forKey: .parameters)
-    // }
-
-    //  func encode(to encoder: Encoder) throws
-    //  {
-    //       var container = encoder.container(keyedBy: ToolInvocationRequestKeys.self)
-    //       try container.encode(toolId, forKey: .toolId)
-    //       try container.encode(parameters, forKey: .parameters)
-    //  }
-}
-
-public struct ToolInvocationResponse: Codable {
-    let result: String
-}
-
 public class MCPClient {
     private let baseURL: URL
 
@@ -42,21 +17,25 @@ public class MCPClient {
         self.baseURL = baseURL
     }
 
-    func invokeTool(toolId: String, parameters: [String: Any]) async throws -> ToolInvocationResponse {
-        let url = baseURL.appendingPathComponent("/tools/\(toolId)/invoke") // endpoint variable
-        let request = URLRequest(url: url)
-        
+    func invokeTool(toolId: String, toolPath: String, parameters: [String: String]) async throws -> String {
+        let url = baseURL.appendingPathComponent(toolPath) // endpoint variable
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
         let toolInvocationRequest = ToolInvocationRequest(toolId: toolId, parameters: parameters)
-        // let body = try encoder.encode(toolInvocationRequest)
-        let body = try toolInvocationRequest.toJson()
+        let encoder = JSONEncoder()
+        let body = try encoder.encode(parameters)
+        
 
         let (data, response) = try await URLSession.shared.upload(for: request, from: body)
 
         guard let httpResponse = response as? HTTPURLResponse, 200...299 ~= httpResponse.statusCode else {
             throw MCPServerError.invalidResponse
         }
-        let decoder = JSONDecoder()
-        let toolInvocationResponse = try decoder.decode(ToolInvocationResponse.self, from: data)
-        return toolInvocationResponse
+        
+        let result = String(data: data, encoding: .utf8) ?? "no response"
+        return result
     }
 }
--- Tests/mcpo-swiftTests/mcpo_swiftTests.swift
+++ Tests/mcpo-swiftTests/mcpo_swiftTests.swift
@@ -19,3 +19,20 @@ import Testing
     }
     #expect(toolDefinitions.count > 0)
 }
+
+@Test func testGetCurrentTime() async throws {
+    let url = [URL(string: "http://micro-atx.local:8084/server-time/openapi.json")!]
+    
+    let toolDefinitions = try await MCPDiscovery.discoverServers(fromURLs: url)
+    print("Found tools: ")
+    let functionTool = toolDefinitions.first { functionTool in
+        functionTool.name == "/get_current_time"
+    }
+
+    guard let functionTool = functionTool else { return }
+    
+    let clientUrl = URL(string: "http://micro-atx.local:8084/server-time")!
+    let client = MCPClient(baseURL: clientUrl)
+    let response = try await client.invokeTool(toolId: "asdf", toolPath: functionTool.name, parameters: ["timezone": "Europe/Madrid"])
+    print("Current time is: \(response)")
+}
